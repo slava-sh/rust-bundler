133aae668ab75db38a8e77092e924fad6dcafed8
Slava Shklyaev <slava@slava.sh>
2018-04-16 00:22:39 +0300

    Add crate expansion

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,106 @@
-extern crate syn;
 extern crate quote;
+extern crate syn;
+extern crate toml;
 
 use std::env;
 use std::fs::File;
 use std::io::Read;
-use std::path::{PathBuf, Path};
+use std::mem;
+use std::path::Path;
 use std::process;
 
 use quote::ToTokens;
-use syn::visit_mut::VisitMut;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("Usage: expand-mods path/to/main.rs");
+        eprintln!("Usage: bundle path/to/project");
         process::exit(1);
     }
-    let path = Path::new(&args[1]);
-    let code = read_file(path).expect("failed to read file");
-    let mut syntax = syn::parse_file(&code).expect("failed to parse file");
-    expand_mods(
-        &mut syntax,
-        path.parent().expect("no parent dir").to_owned(),
-    );
-    println!("{}", &syntax.into_tokens().to_string());
+
+    let project = Path::new(&args[1]);
+
+    let cargo_toml = read_file(&project.join("Cargo.toml")).expect("failed to read Cargo.toml");
+    let cargo_toml: toml::Value = toml::from_str(&cargo_toml).expect("failed to parse Cargo.toml");
+    let package_name = cargo_toml
+        .get("package")
+        .expect("Cargo.toml has no package")
+        .get("name")
+        .expect("Cargo.toml has no package.name")
+        .as_str()
+        .expect("failed to read package name");
+
+    let src = project.join("src");
+    let code = read_file(&src.join("main.rs")).expect("failed to read main.rs");
+    let mut file = syn::parse_file(&code).expect("failed to parse main.rs");
+    bundle(&mut file, package_name, &src);
+    println!("{}", &file.into_tokens().to_string());
 }
 
-fn expand_mods(syntax: &mut syn::File, base_path: PathBuf) {
-    ExpandingVisitor { base_path: base_path }.visit_file_mut(syntax);
+fn bundle(file: &mut syn::File, package_name: &str, base_path: &Path) {
+    expand_crate(file, base_path, package_name);
+    expand_mods(file, base_path);
 }
 
-struct ExpandingVisitor {
-    base_path: PathBuf,
-}
-
-impl VisitMut for ExpandingVisitor {
-    fn visit_item_mod_mut(&mut self, item: &mut syn::ItemMod) {
-        if let Some(ref mut it) = item.content {
-            for it in &mut (it).1 {
-                self.visit_item_mut(it);
+fn expand_crate(file: &mut syn::File, base_path: &Path, crate_name: &str) {
+    let mut crate_item_i = None;
+    for (i, item) in file.items.iter_mut().enumerate() {
+        if let syn::Item::ExternCrate(ref mut item) = *item {
+            if item.ident.to_string() == crate_name {
+                crate_item_i = Some(i);
+                break;
             }
+        }
+    }
+    if let Some(i) = crate_item_i {
+        eprintln!(
+            "expanding crate {} in {}",
+            crate_name,
+            base_path.to_str().unwrap()
+        );
+        let code = read_file(&base_path.join("lib.rs")).expect("failed to read lib.rs");
+        let mut lib = syn::parse_file(&code).expect("failed to parse lib.rs");
+        expand_mods(&mut lib, base_path);
+        let (crates, items) = lib.items.drain(..).partition(is_extern_crate);
+
+        let item = mem::replace(&mut file.items[i], unsafe { mem::uninitialized() });
+        if let syn::Item::ExternCrate(item) = item {
+            file.items[i] = syn::Item::Mod(syn::ItemMod {
+                attrs: item.attrs,
+                vis: item.vis,
+                mod_token: Default::default(),
+                ident: item.ident,
+                content: Some((Default::default(), items)),
+                semi: Some(item.semi_token),
+            });
         } else {
+            unreachable!();
+        }
+
+        let mut new_items = crates;
+        new_items.extend(file.items.drain(..));
+        file.items = new_items;
+    }
+}
+
+fn is_extern_crate(item: &syn::Item) -> bool {
+    if let syn::Item::ExternCrate(_) = *item {
+        true
+    } else {
+        false
+    }
+}
+
+fn expand_mods(file: &mut syn::File, base_path: &Path) {
+    for item in file.items.iter_mut() {
+        if let syn::Item::Mod(ref mut item) = *item {
+            if item.content.is_some() {
+                continue;
+            }
             let name = item.ident.to_string();
             let (base_path, code) = vec![
-                (self.base_path.clone(), format!("{}.rs", name)),
-                (self.base_path.join(&name), String::from("mod.rs")),
+                (base_path.to_owned(), format!("{}.rs", name)),
+                (base_path.join(&name), String::from("mod.rs")),
             ].into_iter()
                 .flat_map(|(base_path, file_name)| {
                     read_file(&base_path.join(file_name)).map(|code| (base_path, code))
@@ -52,9 +108,9 @@ impl VisitMut for ExpandingVisitor {
                 .next()
                 .expect("mod not found");
             eprintln!("expanding mod {} in {}", name, base_path.to_str().unwrap());
-            let mut syntax = syn::parse_file(&code).expect("failed to parse file");
-            expand_mods(&mut syntax, base_path);
-            item.content = Some((Default::default(), syntax.items));
+            let mut file = syn::parse_file(&code).expect("failed to parse file");
+            expand_mods(&mut file, &base_path);
+            item.content = Some((Default::default(), file.items));
         }
     }
 }
